82a622a3c6adec0cf22608712abb28ae4addbe3b
Marc <marc@gmc303.com>
2022-11-22 14:33:57 +0800

    update hash logic

--- hash/hash.go
+++ hash/hash.go
@@ -1,10 +1,40 @@
 package hash
 
-import "hash/crc32"
+import (
+	"crypto/md5"
+	"fmt"
 
-func StringHash(in string) uint32 {
+	"hash/crc32"
+)
+
+var chars string = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+func StringHash(in string) string {
 	h := crc32.NewIEEE()
 	h.Write([]byte(in))
 
-	return h.Sum32()
+	return encode(h.Sum32())
+}
+
+func StringMD5(in string) string {
+	has := md5.Sum([]byte(in))
+	md5str := fmt.Sprintf("%x", has)
+
+	return md5str
+}
+
+func encode(num uint32) string {
+	bytes := []byte{}
+	for num > 0 {
+		bytes = append(bytes, chars[num%62])
+		num = num / 62
+	}
+	reverse(bytes)
+	return string(bytes)
+}
+
+func reverse(a []byte) {
+	for left, right := 0, len(a)-1; left < right; left, right = left+1, right-1 {
+		a[left], a[right] = a[right], a[left]
+	}
 }
