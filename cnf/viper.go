cea2a25a57bcb317e20be4585136e0e8ab19b299
Marc <marc@gmc303.com>
2023-01-11 11:52:17 +0800

    add cnf setting func

---
+++ cnf/viper.go
@@ -0,0 +1,18 @@
+package cnf
+
+import "github.com/spf13/viper"
+
+func SetConfig(path, name, fileType string) *viper.Viper {
+	cnf := viper.New()
+	cnf.AddConfigPath(path)
+	cnf.SetConfigName(name)
+	cnf.SetConfigType(fileType)
+	cnf.AutomaticEnv()
+
+	err := cnf.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	return cnf
+}
