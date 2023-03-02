5dfc94ae48e6dd9682a9bce13d0fa15bd12942ad
Marc <marc@gmc303.com>
2023-03-02 17:51:51 +0800

    pref func Struct2Map

--- conversion/struct.go
+++ conversion/struct.go
@@ -1,16 +1,30 @@
 package conversion
 
 import (
+	"bytes"
 	"encoding/json"
 )
 
-// 將 struct 轉換為 定義的 map
-func Struct2Map(obj interface{}) (data map[string]interface{}) {
-	data = make(map[string]interface{})
+// 將 struct 轉換為定義的 map
+func Struct2Map(data interface{}) map[string]interface{} {
+	var dataMap map[string]interface{}
 
-	j, _ := json.Marshal(obj)
-	json.Unmarshal(j, &data)
-	return
+	if marshalData, err := json.Marshal(data); err != nil {
+		return nil
+	} else {
+		d := json.NewDecoder(bytes.NewReader(marshalData))
+		d.UseNumber() // float64 to number (避免科學記號)
+		if err := d.Decode(&dataMap); err != nil {
+			return nil
+
+		} else {
+			for k, v := range dataMap {
+				dataMap[k] = v
+			}
+		}
+	}
+
+	return dataMap
 }
 
 // 將 來源的 struct 對應至新的 struct
