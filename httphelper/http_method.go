54aad9363e18cfa9f1abc2b7f402bd5cf402ad94
黃正揚 <marc@huangzhengyangdeMacBook-Pro.local>
2023-08-23 15:16:35 +0800

    add post with token

--- httphelper/http_method.go
+++ httphelper/http_method.go
@@ -47,6 +47,43 @@ func SendPostWithJSON(apiHost string, res interface{}, data interface{}, timeOut
 	return nil
 }
 
+func SendPostWithJSONAndToken(apiHost string, res interface{}, data interface{}, token string, timeOut time.Duration) error {
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{Timeout: timeOut}
+	req, err := http.NewRequest("POST", apiHost, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "bearer "+token)
+
+	apiRes, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer apiRes.Body.Close()
+
+	resBody, err := ioutil.ReadAll(apiRes.Body)
+	if err != nil {
+		return err
+	}
+
+	if apiRes.StatusCode != http.StatusOK {
+		return fmt.Errorf("req: %s, res: %s", string(jsonData), string(resBody))
+	}
+
+	err = json.Unmarshal(resBody, &res)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func SendPostWithUrl(apiHost string, res interface{}, data url.Values, timeOut time.Duration) error {
 	// New multiport writer
 	body := &bytes.Buffer{}
