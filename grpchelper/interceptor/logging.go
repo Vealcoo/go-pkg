d0fb34ee435303e4aad149198e25648f506d1b23
Marc <marc@gmc303.com>
2024-05-10 23:41:25 +0800

    grpc logging interceptor add isDebug params

--- grpchelper/interceptor/logging.go
+++ grpchelper/interceptor/logging.go
@@ -7,14 +7,14 @@ import (
 	"google.golang.org/grpc"
 )
 
-func ServerLogging(log zerolog.Logger) grpc.UnaryServerInterceptor {
+func ServerLogging(log zerolog.Logger, isDebug bool) grpc.UnaryServerInterceptor {
 	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo,
 		handler grpc.UnaryHandler) (interface{}, error) {
 		res, err := handler(ctx, req)
 
 		if err != nil {
 			log.Error().Err(err).Str("api", info.FullMethod).Interface("req", req).Interface("res", res).Msg("server return error")
-		} else {
+		} else if isDebug {
 			log.Debug().Str("api", info.FullMethod).Interface("req", req).Interface("res", res).Msg("server return success")
 		}
 
